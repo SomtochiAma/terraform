13bf8885e741df184db584940b5069632a2e6343
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2020-04-22 17:31:04 +0100

    Init files

---
+++ main.tf
@@ -0,0 +1,34 @@
+variable "aws_secret_key" {}
+variable "aws_access_key" {}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-west-2"
+}
+
+resource "aws_key_pair" "example" {
+  key_name   = "examplekey"
+  public_key = file("~/.ssh/terraform.pub")
+}
+
+resource "aws_instance" "example" {
+  key_name      = aws_key_pair.example.key_name
+  ami           = "ami-04590e7389a6e577c"
+  instance_type = "t2.micro"
+
+  # connection {
+  #   type        = "ssh"
+  #   user        = "ec2-user"
+  #   private_key = file("~/.ssh/terraform")
+  #   host        = self.public_ip
+  # }
+
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "sudo amazon-linux-extras enable nginx1.12",
+  #     "sudo yum -y install nginx",
+  #     "sudo systemctl start nginx"
+  #   ]
+  # }
+}
